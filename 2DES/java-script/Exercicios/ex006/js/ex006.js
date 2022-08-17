9ab45fb38e6a7d31ad9eaf6a624523116a9318c3
Futsuiii <89948802+Futsuiii@users.noreply.github.com>
2022-08-17 19:59:12 -0300

    17/08

--- 2DES/java-script/Exercicios/ex006/js/ex006.js
+++ 2DES/java-script/Exercicios/ex006/js/ex006.js
@@ -1,53 +1,64 @@
+var users = [
+
+    {
+      "usuario": "admin",
+      "senha": "admin"  
+    },
+    {
+      "usuario": "luiz",
+      "senha": "luizlindo"  
+    },{
+      "usuario": "senai",
+      "senha": "dev"  
+    }
+
+]
+
+var achou = false
+
+function logar(e) {
+
+  users.forEach(item => {
+    var user = document.querySelector("#email").value
+    var senha = document.querySelector("#senha").value
+
+    if(user == item.usuario && senha == item.senha) {
+      window.location.href = "./page/home.html"
+      console.log(window.location.href)
+      achou = true
+    }
+  }) 
+
+  if(achou == false) {
+    alert('Credenciais erradas')
+  }
+}
+
+
+
 var opcoes = [
 
     {
         "img": "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
         "nome": "Nike Shoes Red",
-        "preco": "169,00",
-        "backColor": '#CB001B'
+        "preco": "169,00"
     },
 
     {
         "img": "https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
         "nome": "Nike blue sky",
-        "preco": "159,00",
-        "backColor": '#008CFF'
-        
+        "preco": "159,00"
     },
 
     {
         "img": "https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
         "nome": "Nike green",
-        "preco": "199,00",
-        "backColor": '#B3E140'
+        "preco": "199,00"
     },
-
-    {
-        "img": "https://wallpaper.dog/large/1001830.jpg",
-        "nome": "Nike green",
-        "preco": "199,00",
-        "backColor": '#191816'
-    },
-
-
-    {
-        "img": "https://wallpaper.dog/large/10890189.png",
-        "nome": "Nike Black",
-        "preco": "189,00",
-        "backColor": '#000000'
-    },
-
-    {
-        "img": "https://wallpaper.dog/large/17173259.jpg",
-        "nome": "Nike selva",
-        "preco": "259,00",
-        "backColor": '#326B42'
-    },
-
 
 ]
 
-var cards = document.querySelector(".card-item")
+var cards = document.querySelectorAl(".card-item")
 
 function carregar() {
     opcoes.forEach(item => {
@@ -57,10 +68,8 @@ function carregar() {
 
         novoCard.querySelector("#imagem-item").src = item.img
         novoCard.querySelector("#name-product").innerHTML = item.nome
-        novoCard.querySelector("#price-product").innerHTML = 'R$' + item.preco
-        novoCard.querySelector("#card").style.background = item.backColor
+        novoCard.querySelector("#price-product").innerHTML = item.preco
 
-
-        document.querySelector("#items").appendChild(novoCard)
+        document.querySelector(".card").appendChild(novoCard)
     })
 }
